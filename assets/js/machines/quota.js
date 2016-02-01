db02373af3bbba29f8b766672a3e95d1b5d102ea
Callum Williams <callum.williams@stfc.ac.uk>
2016-02-01 13:10:22 +0000

    Fix padding issues and improve display

--- assets/js/machines/quota.js
+++ assets/js/machines/quota.js
@@ -21,7 +21,7 @@ var quota = {
 
             for (var key in json_out) {
                 if (json_out[key] < 0) {
-                    json_out[key] = "<span style='font-size:14px'>&infin;</span>";
+                    json_out[key] = 0;
                 }
             }
 
@@ -32,7 +32,7 @@ var quota = {
             userquotasys = json_out["userquotasys"];
 
             userusedvm = json_out["userusedvm"];
-            userusedcpu = json_out["userusedcpu"];
+            userusedcpu = Math.ceil(json_out["userusedcpu"]);
             userusedmem = json_out["userusedmem"];
             userusedsys = json_out["userusedsys"];
 
@@ -42,21 +42,26 @@ var quota = {
             groupquotasys = json_out["groupquotasys"];
 
             groupusedvm = json_out["groupusedvm"];
-            groupusedcpu = json_out["groupusedcpu"];
+            groupusedcpu = Math.ceil(json_out["groupusedcpu"]);
             groupusedmem = json_out["groupusedmem"];
             groupusedsys = json_out["groupusedsys"];
 
             vmavailable = json_out["availablevm"];
-            cpuavailable = json_out["availablecpu"];
+            cpuavailable = json_out["availablecpu"]|0;
             memavailable = json_out["availablemem"];
             sysavailable = json_out["availablesys"];
 
+            availablequotavm = json_out["availablequotavm"];
+            availablequotacpu = json_out["availablequotacpu"];
+            availablequotamem = json_out["availablequotamem"];
+            availablequotasys = json_out["availablequotasys"];
+
             // Sliders
-            $('#cpu-input').attr('max', Math.floor(cpuavailable));
+            $('#cpu-input').attr('max', cpuavailable);
             $('#memory-input').attr('max', memavailable);
 
             $("#cpu-min").html("1");
-            $("#cpu-max").html(Math.floor(cpuavailable));
+            $("#cpu-max").html(cpuavailable);
 
             $("#mem-min").html("0.5 GB");
             $("#mem-max").html(memavailable + " GB");
@@ -67,9 +72,10 @@ var quota = {
                 'bgColor'     : '#EEEEEE',
                 'min'         : '0',
                 'readOnly'    : 'true',
-                'width'       : '70',
+                'width'       : '90%',
                 'height'      : '70',
                 'thickness'   : '.20',
+                'displayMax'  : 'true'
             };
 
             colour_vm = {
@@ -89,91 +95,74 @@ var quota = {
                 'fgColor'     : '#b40053'
             };
 
+
             // Available
             $('.available-vm').val(vmavailable).trigger('change');
             $(".available-vm").knob($.extend({
-                'max': vmavailable
+                'max': availablequotavm,
+                'rotation': 'anticlockwise'
             }, dial, colour_vm));
 
             $('.available-cpu').val(cpuavailable).trigger('change');
             $(".available-cpu").knob($.extend({
-                'max': cpuavailable
+                'max': availablequotacpu,
+                'rotation': 'anticlockwise'
             }, dial, colour_cpu));
 
             $('.available-mem').val(memavailable).trigger('change');
             $(".available-mem").knob($.extend({
-                'max': memavailable
+                'max': availablequotamem,
+                'rotation': 'anticlockwise'
             }, dial, colour_mem));
 
             $('.available-sys').val(sysavailable).trigger('change');
             $(".available-sys").knob($.extend({
-                'max': sysavailable
+                'max': availablequotasys,
+                'rotation': 'anticlockwise'
             }, dial, colour_sys));
+
 
             // User
             $('.user-vm').val(userusedvm).trigger('change');
             $(".user-vm").knob($.extend({
-                'max': userquotavm,
-                'format'      : function (value) {
-                    return value + "/" + userquotavm;
-                }
+                'max': userquotavm
             }, dial, colour_vm));
 
             $('.user-cpu').val(userusedcpu).trigger('change');
             $(".user-cpu").knob($.extend({
-                'max': userquotacpu,
-                'format'      : function (value) {
-                    return value + '/' + userquotacpu;
-                }
+                'max': userquotacpu
             }, dial, colour_cpu));
 
             $('.user-mem').val(userusedmem).trigger('change');
             $(".user-mem").knob($.extend({
-                'max': userquotamem,
-                'format'      : function (value) {
-                    return value + '/GB' + userquotamem;
-                }
+                'max': userquotamem
             }, dial, colour_mem));
 
             $('.user-sys').val(userusedsys).trigger('change');
             $(".user-sys").knob($.extend({
-                'max': userquotasys,
-                'format'      : function (value) {
-                    return value + '/' + userquotasys + 'GB';
-                }
+                'max': userquotasys
             }, dial, colour_sys));
+
 
             // Group
             $('.group-vm').val(groupusedvm).trigger('change');
             $(".group-vm").knob($.extend({
-                'max': groupquotavm,
-                'format'      : function (value) {
-                    return value + '/' + groupquotavm;
-                }
+                'max': groupquotavm
             }, dial, colour_vm));
 
             $('.group-cpu').val(groupusedcpu).trigger('change');
             $(".group-cpu").knob($.extend({
-                'max': groupquotacpu,
-                'format'      : function (value) {
-                    return value + '/' + groupquotacpu;
-                }
+                'max': groupquotacpu
             }, dial, colour_cpu));
 
             $('.group-mem').val(groupusedmem).trigger('change');
             $(".group-mem").knob($.extend({
-                'max': groupquotamem,
-                'format'      : function (value) {
-                    return value + '/' + groupquotamem;
-                }
+                'max': groupquotamem
             }, dial, colour_mem));
 
             $('.group-sys').val(groupusedsys).trigger('change');
             $(".group-sys").knob($.extend({
-                'max': groupquotasys,
-                'format'      : function (value) {
-                    return value + '/' + groupquotasys + 'GB';
-                }
+                'max': groupquotasys
             }, dial, colour_sys));
         });
     }
