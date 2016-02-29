b48931c38a4064dc62f18e6f82451a6836f00146
Callum Williams <callum.williams@stfc.ac.uk>
2016-02-29 10:39:22 +0000

    Add news/alerts banner

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -19,3 +19,34 @@ $(function() {
         window.location.replace("/");
     });
 });
+
+// Messages
+function notes() {
+    $('#site-messages').load('/assets/dynamic/messages.html');
+}
+notes();
+window.setInterval(notes, 6000);
+
+function close_alert(id) {
+    Cookies.set(id + '-msg', 'hidden', {expires : 1, path : '/'});
+}
+function note_all(id, type, title, body) {
+    if (!Cookies.get(id + '-msg')) {
+        output = '<div id="' + id + '" class="alert alert-' + type + ' alert-dismissible global-alert" role="alert">';
+        output += '<h4>' + title;
+        output += '<button onClick="close_alert(' + id + ')" type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span><span class="text"> Dismiss</span></button></h4>';
+        output += '<p>' + body + '</p><span class="signature">~ The Cloud Team</span><br>';
+        output += '</div></div>';
+        $('#site-messages').append(output);
+    }
+}
+function note_members(id, type, title, body) {
+    if (Cookies.get('name') && !Cookies.get(id + '-msg')) {
+        output = '<div id="' + id + '" class="alert alert-' + type + ' alert-dismissible global-alert" role="alert">';
+        output += '<h4>' + title;
+        output += '<button onClick="close_alert(' + id + ')" type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span><span class="text"> Dismiss</span></button></h4>';
+        output += '<p>' + body + '</p><span class="signature">~ The Cloud Team</span><br>';
+        output += '</div></div>';
+        $('#site-messages').append(output);
+    }
+}
