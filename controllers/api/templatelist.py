6b4140c9dd1cdcc064f6b61e6ebeb0e7e074c304
MRichards99 <matthew.richards@stfc.ac.uk>
2017-12-05 16:08:12 +0000

    Add simple 500 error checking for image metadata

--- controllers/api/templatelist.py
+++ controllers/api/templatelist.py
@@ -24,21 +24,25 @@ class TemplateList(object):
 	# os_distro - name of flavor
 	menuchoices = defaultdict(lambda: defaultdict(dict))
 	for image in novaClient.images.list():
-	    if image.metadata[u'os_distro'] != None:
-		osDistro = image.metadata[u'os_distro']
-	    else:
-		cherrypy.log("has an uninstantiated image flavour", username)
-		continue
-	    if image.metadata[u'os_version'] != None:
-                osVersion = image.metadata[u'os_version']
-            else:
-                cherrypy.log("has an uninstantiated image version", username)
-                continue
-	    if image.metadata[u'os_variant'] != None:
-                osVariant = image.metadata[u'os_variant']
-            else:
-                cherrypy.log("has an uninstantiated image variant", username)
-                continue
+            try:
+                if image.metadata[u'os_distro'] != None:
+		    osDistro = image.metadata[u'os_distro']
+	        else:
+		    cherrypy.log("has an uninstantiated image flavour", username)
+		    continue
+    	        if image.metadata[u'os_version'] != None:
+                    osVersion = image.metadata[u'os_version']
+                else:
+                    cherrypy.log("has an uninstantiated image version", username)
+                    continue
+	        if image.metadata[u'os_variant'] != None:
+                    osVariant = image.metadata[u'os_variant']
+                else:
+                    cherrypy.log("has an uninstantiated image variant", username)
+                    continue
+            except:
+                raise cherrypy.HTTPError(500)
+
             try:
                 if image.metadata[u'aq_managed'] == "false":
                     aqManaged = "false"
