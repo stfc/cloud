197b49213ced4718e85a77e4858104ec57c54fe1
MRichards99 <matthew.richards@stfc.ac.uk>
2017-12-06 13:24:23 +0000

    Add file getFunctions.py
    - This file will contain commonly used methods that interact with Nova & Keystone APIs

---
+++ controllers/api/getFunctions.py
@@ -0,0 +1,25 @@
+import cherrypy
+from keystoneauth1 import session
+from keystoneauth1.identity import v3
+import novaclient.client as nClient
+
+def getNovaInstance():
+
+    NOVA_VERSION = cherrypy.request.config.get("novaVersion")
+    KEYSTONE_URL = cherrypy.request.config.get("keystone")
+    OPENSTACK_DEFAULT_DOMAIN = cherrypy.request.config.get("openstack_default_domain")
+
+    # Creating instance of Nova
+    projectName = "admin"
+    projectAuth = v3.Password(
+        auth_url = KEYSTONE_URL,
+        username = cherrypy.session['username'],
+        password = cherrypy.session['password'],
+        user_domain_name = OPENSTACK_DEFAULT_DOMAIN,
+        project_id = cherrypy.request.cookie.get('projectID').value,
+        project_domain_name = OPENSTACK_DEFAULT_DOMAIN
+    )
+    sess = session.Session(auth=projectAuth, verify='/etc/ssl/certs/ca-bundle.crt')
+    return nClient.Client(NOVA_VERSION, session = sess)
+
+
