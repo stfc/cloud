fb8e99c273a1a7da226cb274113493d7e0840ee8
MRichards99 <matthew.richards@stfc.ac.uk>
2018-01-03 09:46:36 +0000

    Generally improve quota.py file
    - Converted tabs to spaces
    - Added some error checking (quotaData)
    - General increase in comments

--- controllers/api/quota.py
+++ controllers/api/quota.py
@@ -9,63 +9,69 @@ class Quota(object):
     def GET(self):
         novaClient = getNovaInstance()
 
-	# Getting project limits from Nova
+        # Getting project limits from Nova
         try:
             projectLimits = novaClient.limits.get().absolute
         except:
-	    raise cherrypy.HTTPError('500 OpenStack cannot get the quota values.')
+            raise cherrypy.HTTPError('500 OpenStack cannot get the quota values.')
 
-	# Converting Nova limits to a dictionary
-	l = list(projectLimits)
-	limits = dict(map(lambda x: (x.name, x.value), l))
+        # Converting Nova limits to a dictionary
+        l = list(projectLimits)
+        limits = dict(map(lambda x: (x.name, x.value), l))
 
-	quotaData = {
-            'groupquotamem' : limits['maxTotalRAMSize']/1024,
-            'groupquotacpu' : limits['maxTotalCores'],
-            'groupquotavm' : limits['maxTotalInstances'],
+        try:
+            quotaData = {
+                'groupquotamem' : limits['maxTotalRAMSize']/1024,
+                'groupquotacpu' : limits['maxTotalCores'],
+                'groupquotavm' : limits['maxTotalInstances'],
 
-	    'groupusedmem' : limits['totalRAMUsed']/1024,
-	    'groupusedcpu' : limits['totalCoresUsed'],
-	    'groupusedvm'  : limits['totalInstancesUsed'],
-        }
+                'groupusedmem' : limits['totalRAMUsed']/1024,
+                'groupusedcpu' : limits['totalCoresUsed'],
+                'groupusedvm'  : limits['totalInstancesUsed'],
+            }
+        except AttributeError:
+            raise cherrypy.HTTPError('500 There has been an AttributeError putting quota data into a dictionary to be manipulated.')
 
-	quotaDataKeys = []
-	# Available quota figures only used when there are limits on quotas
-	if quotaData['groupquotamem'] > -1:
-	    quotaData['availablequotamem'] = quotaData['groupquotamem'] - quotaData['groupusedmem']
-	else:
-	    quotaDataKeys.append('biggestRAMAmount')
-	if quotaData['groupquotacpu'] > -1:
-	    quotaData['availablequotacpu'] = quotaData['groupquotacpu'] - quotaData['groupusedcpu']
-	else:
-	    quotaDataKeys.append('biggestCPUAmount')
-	if quotaData['groupquotavm'] > -1:
-	    quotaData['availablequotavm'] = quotaData['groupquotavm'] - quotaData['groupusedvm']
-	quotaDataKeys.append('biggestDiskAmount')
+        quotaDataKeys = []
+        # Available quota figures only used when there are limits on quotas
+        if quotaData['groupquotamem'] > -1:
+            quotaData['availablequotamem'] = quotaData['groupquotamem'] - quotaData['groupusedmem']
+        else:
+            quotaDataKeys.append('biggestRAMAmount')
+        if quotaData['groupquotacpu'] > -1:
+            quotaData['availablequotacpu'] = quotaData['groupquotacpu'] - quotaData['groupusedcpu']
+        else:
+            quotaDataKeys.append('biggestCPUAmount')
+        if quotaData['groupquotavm'] > -1:
+            quotaData['availablequotavm'] = quotaData['groupquotavm'] - quotaData['groupusedvm']
 
-	# Finding biggest disk size from each flavor to use in disk slider
-	ramList = []
-	cpuList = []
-	diskList = []
-	for flavor in novaClient.flavors.list():
+        # Always appended as there's no disk quota - biggest disk size will always be required
+        quotaDataKeys.append('biggestDiskAmount')
+
+        ramList = []
+        cpuList = []
+        diskList = []
+        # Searching each flavor to put each attribute into an indivdual list
+        # Biggest value found a few lines below - this is used for sliders when creating a VM
+        for flavor in novaClient.flavors.list():
             if quotaData['groupquotamem'] == -1:
-		ramList.append(flavor.ram)
+                ramList.append(flavor.ram)
             if quotaData['groupquotacpu'] == -1:
                 cpuList.append(flavor.vcpus)
             diskList.append(flavor.disk)
 
-	biggestAmountList = []
-	# Puts each list into biggestAmountList to find max. value of each inner list
-	self.listLengthCheck(ramList, biggestAmountList)
-	self.listLengthCheck(cpuList, biggestAmountList)
-	self.listLengthCheck(diskList, biggestAmountList)
+        biggestAmountList = []
+        # Puts each list into biggestAmountList to find max. value of each inner list
+        self.listLengthCheck(ramList, biggestAmountList)
+        self.listLengthCheck(cpuList, biggestAmountList)
+        self.listLengthCheck(diskList, biggestAmountList)
 
-	if len(quotaDataKeys) > 0:
+        if len(quotaDataKeys) > 0:
             for i in range (0, len(quotaDataKeys)):
-	        quotaData[quotaDataKeys[i]]= max(biggestAmountList[i])
+                quotaData[quotaDataKeys[i]]= max(biggestAmountList[i])
 
-	return quotaData
+        return quotaData
 
     def listLengthCheck(self, resourceList, biggestAmountList):
         if len(resourceList) != 0:
-	    biggestAmountList.append(resourceList)
+            biggestAmountList.append(resourceList)
