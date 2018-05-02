e64f3e47c7153de7b9e79ec32762807f406e12a0
JHaydock-Pro <jack.haydock@stfc.ac.uk>
2018-05-02 18:25:51 +0100

    Correction to prev. commit

---
+++ controllers/api/openstack/vnc.py
@@ -0,0 +1,48 @@
+import cherrypy
+from datetime import datetime
+from time import mktime
+
+from novaclient.exceptions import ClientException, NotFound
+
+from getFunctions import getNovaInstance
+
+class VNC(object):
+    '''
+        Return JSON list of VM regarding vnc consoles 
+    '''
+    exposed = True
+    @cherrypy.tools.isAuthorised()
+    @cherrypy.tools.json_out()
+    def GET(self):
+	novaClient = getNovaInstance()
+        username = cherrypy.request.cookie.get('fedid').value
+
+	json = []	
+
+        for server in novaClient.servers.list(detailed = True):
+            # Gets URL with VNC token embedded
+            if server.status == "ACTIVE":
+		try:
+		    vncURL = server.get_vnc_console(console_type = "novnc")[u'console'][u'url']
+		    vncToken = self.cutString(vncURL, 62, len(vncURL))
+		except ClientException as e:
+                    cherrypy.log(username + ' - ' + str(type(e)) + ' when retrieving VNC details for ' + server.name);
+                    cherrypy.log(str(e));
+		    vncURL = ""
+		    vncToken = ""
+	    else:
+		vncURL = ""
+                vncToken = ""
+
+	    # Put VM data into json format for .js file
+	    json.append({
+		'id'       : server.id,
+                'token'    : vncToken,
+		'vncURL'   : vncURL,
+	    })
+	return {"data":json}
+
+
+    def cutString(self, string, start, end):
+	return string[start:end]
+
