daa425495063022854ea68d837247aa0307a9036
Edward <edward.thompson@stfc.ac.uk>
2021-03-25 16:25:02 +0000

    Create magnum access methods

--- controllers/api/openstack/getFunctions.py
+++ controllers/api/openstack/getFunctions.py
@@ -6,6 +6,10 @@ import novaclient.client as nClient
 from novaclient.exceptions import ClientException
 
 import glanceclient.client as gClient
+import magnumclient.client as mClient
+
+from subprocess import run
+import re
 
 def getGlanceInstance():
     GLANCE_VERSION = '2'
@@ -33,6 +37,41 @@ def getNovaInstance():
         cherrypy.log('- Error when creating nova client instance', username, traceback=True)
         raise cherrypy.HTTPError('500 There\'s been an error when logging you in')
     return client
+
+def getMagnumInstance():
+    MAGNUM_VERSION = '1'
+    username = cherrypy.session['username']
+
+    sess = getOpenStackSession(include_project_id=True)
+
+    try:
+        client = mClient.Client(MAGNUM_VERSION, session = sess)
+    except ClientException as e:
+        cherrypy.log('- Error when creating magnum client instance', username, traceback=True)
+        raise cherrypy.HTTPError('500 There\'s been an error when logging you in')
+    return client
+
+def getClusterConfig(uuid, path):
+    """
+    Retrieves the config file for the given cluster.
+    Stores the file in the given path.
+    """
+    # NOTE: The CLI command is used here as an equivalent command within a python library
+    #   could not be found. If one is found this method can be replaced.
+    cli_cmd = f"openstack coe cluster config {uuid}" \
+              f" --dir={path} --force" \
+              f" --os-auth-url '{cherrypy.request.config.get('keystone')}'" \
+              f" --os-username '{cherrypy.session['username']}'" \
+              f" --os-password '{cherrypy.session['password']}'" \
+              f" --os-user-domain-name '{cherrypy.request.config.get('openstack_default_domain')}'" \
+              f" --os-project-id '{cherrypy.request.cookie.get('projectID').value}'"
+
+    cp = run(cli_cmd, shell=True, capture_output=True)  # Run the command and captures the 'CompletedProcess'
+    stdout_str = cp.stdout.decode('utf-8')  # Extracts the output as a string. For the command run, this should be the config path.
+    
+    CCFG_REGEX = r'(?<=KUBECONFIG=).*'  # A regex to extract the cluster-config-file from the output
+    config_path = re.search(CCFG_REGEX, stdout_str).group(0)
+    return config_path
 
 def getOpenStackSession(include_project_id=False):
     # Getting relevant details from config/global.conf
